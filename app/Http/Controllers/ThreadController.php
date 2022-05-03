47213c00454e44dc824b151c6564551139abee9f
hash52 <hirokinoakaunto@gmail.com>
2022-05-03 01:32:42 +0000

    スレッド投稿

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Thread;
+
+class ThreadController extends Controller
+{
+    public function add()
+    {
+        return view('threads.new');
+    }
+    
+    public function create(Request $request)
+    {
+        $thread = new Thread;
+        $thread->fill($request->all());
+        $thread->save();
+        return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'threads', 'middleware'=>'auth'], function () {
+    Route::get('new', 'ThreadController@add');
+    Route::post('/', 'ThreadController@create');
+});
